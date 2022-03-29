43b88f72e7dfea84a07ccda6aabb429f2b4093c8
Sawyerf <sawyer.flink@protonmail.ch>
2022-03-29 19:01:56 +0200

    Init Project

---
+++ app/src/main/java/com/example/ft_hangouts/controller/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.ft_hangouts.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ft_hangouts.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
